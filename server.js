cd64e1b8d57f2855c7306bd8cd0f909938ee0c74
emeery <gerardoemery@live.com.mx>
2018-11-14 20:06:23 -0600

    portafolio

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view-engine', 'hbs');
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     var ahora = new Date().toString();
     console.log(`${ahora} ${req.method} ${req.url}`); //log
     next();
@@ -34,9 +34,14 @@ app.get('/acerca', (req, res) => {
         titulo: 'Acerca Pagina'
     });
 });
+app.get('/proyectos', (req, res) => {
+    res.render('proyectos.hbs', {
+        titulo: 'Proyectos'
+    });
+});
 app.get('/error', (req, res) => {
     res.send({
-        errorMensaje: 'no se obtener la solicitud'
+        errorMensaje: 'no se puede obtener la solicitud'
     });
 });
 app.listen(port, () => {
