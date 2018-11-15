ee90ad91a42d87ec1390568ec15851f62c42d995
emeery <gerardoemery@live.com.mx>
2018-11-14 19:49:53 -0600

    heroku

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-var app = express();
 const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view-engine', 'hbs');
-app.use(express.static(__dirname + '/public')); //estáticos
 
+app.use((req, res, next) => {
+    var ahora = new Date().toString();
+    console.log(`${ahora} ${req.method} ${req.url}`); //log
+    next();
+});
 // app.use((req, res, next) => {
 //     res.render('mantenimiento.hbs');
 // });
+app.use(express.static(__dirname + '/public')); //estáticos
+
 hbs.registerHelper('getAñoActual', () => {
     return new Date().getFullYear()
 });
@@ -31,4 +39,6 @@ app.get('/error', (req, res) => {
         errorMensaje: 'no se obtener la solicitud'
     });
 });
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`puerto ${port}`);
+});
